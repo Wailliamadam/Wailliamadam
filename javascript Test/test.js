3ccf338e4c1d2ee98df3dd1ec2ac5dbdd1f02a28
Wai Yan <waung6486@gmail.com>
2023-06-26 19:46:06 +0630

    Update test.js
    testing java script

--- javascript Test/test.js
+++ javascript Test/test.js
@@ -1,2 +1,20 @@
+let money = 60000000;
+console.log(money)
+let count = 56 ;
+console.log(count)
+let goo = "foo"
+console.log(goo)
 let name = "Wai Yan Aung" ; 
 console.log(name)
+console.log(56 + 67) ; 
+console.log(76 - 23) ;
+
+let numOne = 56;
+let numTwo = 67 ;
+if(numOne > numTwo) {
+    console.log("Num One is Big") ;
+}else {
+    console.log("Num Two is Big") ;
+  }
+
+
